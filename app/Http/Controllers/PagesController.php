7869f8c3704e7bf8450efacc7cae6408bc543c6f
cursedchocken <kevinpalinckx12@gmail.com>
2023-11-17 12:20:04 +0100

    de Pagecontroller en web.php
    de PagesController.php en de web.php is toegevoegt

--- app/Http/Controllers/PagesController.php
+++ app/Http/Controllers/PagesController.php
@@ -6,5 +6,13 @@ use Illuminate\Http\Request;
 
 class PagesController extends Controller
 {
-    //
+    public function events()
+    {
+        return view('pages/events');
+    }
+
+    public function testroute()
+    {
+        return view('pages/testroute');
+    }
 }
