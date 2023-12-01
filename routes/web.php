fa50cc01a081f0b33006151e89538d777f0427d4
Michel Escaravage <108881977+MichelEscaravage@users.noreply.github.com>
2023-12-01 12:09:32 +0100

    Added a CRUD function for EventsController

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\EventsController;
 use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\PagesController;
 use Illuminate\Support\Facades\Route;
@@ -24,6 +25,10 @@ Route::get('/', function () {
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::group(['prefix' => 'dashboard', 'middleware' => 'auth'], function(){
+    Route::resource('events', EventsController::class);
+});
 
 Route::get('/testroute',function(){return view('testroute');});
 
